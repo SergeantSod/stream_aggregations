5d83d071b74c10c2124453887ff7cc8bc698663c
Johannes Pirkl <johannespirkl@gmx.de>
2017-07-11 15:54:33 +0200

    Use loan pattern to make sure input is closed again.

--- src/main/scala/stream_aggregations/input/package.scala
+++ src/main/scala/stream_aggregations/input/package.scala
@@ -3,6 +3,13 @@ package stream_aggregations
 import scala.io.Source
 
 package object input {
-  //TODO this doesn't close the file, use loan pattern instead.
-  def openFile(pathName: String):TraversableOnce[String]= Source.fromFile(pathName).getLines()
+
+  def readingFrom(pathName: String)(block: TraversableOnce[String] => Unit):Unit={
+    val source = Source.fromFile(pathName)
+    try {
+      block(source.getLines())
+    } finally {
+      source.close()
+    }
+  }
 }
--- src/test/scala/stream_aggregations/InputTest.scala
+++ src/test/scala/stream_aggregations/InputTest.scala
@@ -7,15 +7,30 @@ class InputTest extends UnitSpec with TempFileSpec {
 
   "The input package" - {
 
-     //TODO Consider using a prop spec instead of hard-coding some example values here. Not sure how slow that is if we actually create temp files
-     val firstLine = "some content for the first line"
-     val secondLine = "some different content for the second line"
+    "when reading from a file" - {
 
-     val fileWithContent = createTempFileWithText(s"$firstLine\n$secondLine")
+      val firstLine = "some content for the first line"
+      val secondLine = "some different content for the second line"
 
-      "should open files and return the lines" in {
-        input.openFile(fileWithContent.getCanonicalPath).toList should ===(List(firstLine, secondLine))
+      //TODO Consider using a prop spec instead of hard-coding some example values here. Not sure how slow that is if we actually create temp files
+      val fileWithContent = createTempFileWithText(s"$firstLine\n$secondLine")
+
+      "should pass the lines to a lambda" in {
+        input.readingFrom(fileWithContent.getCanonicalPath){ lines =>
+          lines.toList should ===(List(firstLine, secondLine))
+        }
       }
+
+      "should pass control to a lambda" in {
+        var called = false
+
+        input.readingFrom(fileWithContent.getCanonicalPath){ _ => called = true }
+        called should be(true)
+      }
+    }
+
+
+
   }
 
 }
