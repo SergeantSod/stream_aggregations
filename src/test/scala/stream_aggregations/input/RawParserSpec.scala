aa7b59beadfcf29ed2d86cdb20867bf4da0765a2
Johannes Pirkl <johannespirkl@gmx.de>
2017-07-13 14:18:50 +0200

    Throw in specs to emphasize that a parser can be mapped over.

--- src/test/scala/stream_aggregations/input/RawParserSpec.scala
+++ src/test/scala/stream_aggregations/input/RawParserSpec.scala
@@ -11,6 +11,26 @@ class RawParserSpec extends UnitSpec {
         Raw.parse(line) should ===(line)
       }
     }
+
+    "when mapped to a different data type via a partial function" - {
+
+      val booleanParser: LineParser[Boolean] = Raw.map{
+        case "true" => true
+        case "false" => false
+      }
+
+      "should parse well-formed strings" in {
+        booleanParser.parse("true") should ===(true)
+        booleanParser.parse("false") should ===(false)
+      }
+
+      "should throw an exception for malformed strings" in {
+        a[ParseException] should be thrownBy{
+          booleanParser.parse("something funky")
+        }
+      }
+
+    }
   }
 
 }
